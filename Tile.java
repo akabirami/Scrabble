e97a2362c77033e602a64d130b5ccbb9cb4bf5b0
amrpandey <amrpandey@paypal.com>
2016-08-05 12:37:33 +0530

    Added Tile class

---
+++ Tile.java
@@ -0,0 +1,17 @@
+public class Tile {
+	
+	private char character;
+	
+	private static final int[] CHARACTER_VALUE  = new int[]{1, 3, 3, 2, 1, 4, 2, 4, 1, 1, 5, 4, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+	
+	
+	public Tile(char character, int value){
+		this.character = character;
+		
+	}
+	
+	public static int getValue(char character){
+		return CHARACTER_VALUE[(int)character - 65];
+	}
+	
+}
