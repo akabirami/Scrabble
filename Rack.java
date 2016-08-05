8e2967e3280772aa51d416b99fe4f4a3bc7abde2
amrpandey <amrpandey@paypal.com>
2016-08-05 13:52:15 +0530

    Rack logic

--- Rack.java
+++ Rack.java
@@ -1,74 +1,56 @@
-package scrabble;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Rack {
 
 	private ArrayList<Tile> rack;
 	private static int score = Integer.MIN_VALUE;
-	
-	public Rack(ArrayList<Tile> rack) {
-		this.rack = rack;
-	}
-	
+	private static HashMap<String, String> anagram;
 
-	int getScore(int[] data) {
-		int score = 0;
-		
-		
-		return score;
-	}
-	 
-	void combinationUtil(int arr[], int data[], int start, int end, int index, int r)  {
-	        // Current combination is ready to be printed, print it
-	        if (index == r) {
-	        	score = Integer.max(score, getScore(data));
-	        	return;
-	        }
-	        for (int i=start; i<=end && end-i+1 >= r-index; i++)  {
-	            data[index] = arr[i];
-	            combinationUtil(arr, data, i+1, end, index+1, r);
-	        }
-	    }
-		 
-		void getMax(int arr[], int n, int r) 	{
-		    int data[]=new int[r];
-		    ArrayList<Tile> substr = new ArrayList<Tile>();
-		    ArrayList<Integer> score = new ArrayList<Integer>();
-		    combinationUtil(arr, data, 0, n-1, 0, r);
+	public Rack(String s) {
+		ArrayList<Tile> rack = new ArrayList<Tile>();
+		for (int i = 0; i < s.length(); i++) {
+			Tile tile = new Tile(s.charAt(i));
+			rack.add(tile);
 		}
-		
-		
-		public int getScore() {
-			int score = 0;
-			
-			return score;
-		}
-	private String getString() {
-		String s = "";
-		for(Tile t: rack) {
-			s += String.valueOf(t.character);
-		}
-		return s;
-	}
-	
-	private static void permutation(String str) {
-	    permutation("", str); 
 	}
 
-	private static void permutation(String prefix, String str) {
-	    int n = str.length();
-	    if (n == 0) System.out.println(prefix);
-	    else {
-	        for (int i = 0; i < n; i++)
-	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-	    }
-	}
-		
-	public int maxValue() {
-		String seq = getString();
+	private int getScore(int[] index) {
 		int score = 0;
-		
+		for (int i = 0; i < index.length; i++) {
+			score += rack.get(i).getValue();
+		}
 		return score;
+	}
+
+	private void getSubScore(int arr[], int data[], int start, int end, int index, int r) {
+		// Current combination is ready to be printed, print it
+		if (index == r) {
+			for (int i = 0; i < r; i++)
+				System.out.print(data[i] + " ");
+			System.out.println();
+			// score = Integer.max(score, getScore(data));
+		}
+		for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
+			data[index] = arr[i];
+			getSubScore(arr, data, i + 1, end, index + 1, r);
+		}
+	}
+
+	int getMaxScore() {
+		int arr[] = { 0, 1, 2, 3, 4, 5, 6 };
+		int data[] = new int[7];
+		createAnagramMap();
+		for (int r = 7; r >= 1; r--) {
+			getSubScore(arr, data, 0, 6, 0, r);
+			if (score != Integer.MIN_VALUE)
+				return score;
+		}
+		return score;
+	}
+
+	private void createAnagramMap() {
+		// TODO Auto-generated method stub
+		
 	}
 }
