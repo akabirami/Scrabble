6c6a57d9cf44d9574cc06054fa0f0b4f7b927e67
amrpandey <amrpandey@paypal.com>
2016-08-05 14:52:00 +0530

    bug fixed in Rack.java

--- Rack.java
+++ Rack.java
@@ -1,62 +1,58 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class AnagramUtil {
+public class Rack {
 
-	public static void main(String[] args) {
-		
-		Map<String,String> anagram=getMapOfWords();
-		System.out.println(printAnargam(anagram));
-	}
-		
-		
-	private static String printAnargam(Map<String, String> anagram) {
-		// TODO Auto-generated method stub
-		String clusters="";
-		for(String s: anagram.keySet()){
-			if(anagram.get(s).contains(" ")){
-				clusters+=anagram.get(s)+"\n";
-			}
+	private ArrayList<Tile> rack;
+	private static int score = Integer.MIN_VALUE;
+	private static Map<String, String> anagram;
+
+	public Rack(String s) {
+		ArrayList<Tile> rack = new ArrayList<Tile>();
+		for (int i = 0; i < s.length(); i++) {
+			Tile tile = new Tile(s.charAt(i));
+			rack.add(tile);
 		}
-		return clusters;
-		
 	}
 
-	public static Map<String,String> getMapOfWords(){
-		 Map<String,String> words_count = new HashMap<String,String>();
-			
-			try {
-				FileInputStream fstream = new FileInputStream("sowpods.txt");
-				BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-
-				String strLine;
-				while ((strLine = br.readLine()) != null)   {
-				  
-					String sorted=sortLetters(strLine);
-				     
-					     if(words_count.keySet().contains(sorted))
-					     {
-					    	 
-					         words_count.put(sorted, words_count.get(sorted)+"  "+strLine);
-					     }
-					     else
-					         words_count.put(sorted,strLine);
-
-					}
-				
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		return words_count;
-		
+	private int getScore(int[] index) {
+		int score = 0;
+		for (int i = 0; i < index.length; i++) {
+			score += rack.get(i).getValue();
+		}
+		return score;
 	}
 
-	private static String sortLetters(String strLine) {
+	private void getSubScore(int arr[], int data[], int start, int end, int index, int r) {
+		// Current combination is ready to be printed, print it
+		if (index == r) {
+			for (int i = 0; i < r; i++)
+				System.out.print(data[i] + " ");
+			System.out.println();
+			// score = Integer.max(score, getScore(data));
+		}
+		for (int i = start; i <= end && end - i + 1 >= r - index; i++) {
+			data[index] = arr[i];
+			getSubScore(arr, data, i + 1, end, index + 1, r);
+		}
+	}
+
+	int getMaxScore() {
+		int arr[] = { 0, 1, 2, 3, 4, 5, 6 };
+		int data[] = new int[7];
+		createAnagramMap();
+		for (int r = 7; r >= 1; r--) {
+			getSubScore(arr, data, 0, 6, 0, r);
+			if (score != Integer.MIN_VALUE)
+				return score;
+		}
+		return score;
+	}
+
+	private void createAnagramMap() {
 		// TODO Auto-generated method stub
-		char[] chars = strLine.toCharArray();
-        Arrays.sort(chars);
-        String sorted = new String(chars).toLowerCase();
-		return sorted;
+		
+		anagram = AnagramUtil.getMapOfWords();
+		
 	}
 }
